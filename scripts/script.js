6165bf55ee7317e6505e6f02ba58fab2f291c451
Alonzo Velez <alonzovelez9@gmail.com>
2023-02-03 19:58:18 -0500

    Removed nonfunctional sqrt & ln buttons from bottom. Added "^" button + functionality.

--- scripts/script.js
+++ scripts/script.js
@@ -1,200 +1,217 @@
-let displayValue = '0';
+let displayValue = "0";
 let firstOperand = null;
 let secondOperand = null;
 let firstOperator = null;
 let secondOperator = null;
 let result = null;
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 console.log("Amount of buttons is:", buttons.length);
 
-window.addEventListener('keydown', function(e){
-    const key = document.querySelector(`button[data-key='${e.keyCode}']`);
-    key.click();
+window.addEventListener("keydown", function (e) {
+  const key = document.querySelector(`button[data-key='${e.keyCode}']`);
+  key.click();
 });
 
 function updateDisplay() {
-    console.log("Inside updateDisplay displayValue is: ", displayValue);
-    const display = document.getElementById('display');
-    display.innerText = displayValue;
-    if(displayValue.length > 9) {
-        display.innerText = displayValue.substring(0, 9);
-    }
+  console.log("Inside updateDisplay displayValue is: ", displayValue);
+  const display = document.getElementById("display");
+  display.innerText = displayValue;
+  if (displayValue.length > 9) {
+    display.innerText = displayValue.substring(0, 9);
+  }
 }
-  
+
 updateDisplay();
 
 function clickButton() {
-    console.log("Inside clickButton");
-    for(let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function() {
-            if(buttons[i].classList.contains('operand')) {
-                inputOperand(buttons[i].value);
-                updateDisplay();
-            } else if(buttons[i].classList.contains('operator')) {
-                inputOperator(buttons[i].value);
-            } else if(buttons[i].classList.contains('equals')) {
-                inputEquals();
-                updateDisplay();
-            } else if(buttons[i].classList.contains('decimal')) {
-                inputDecimal(buttons[i].value);
-                updateDisplay();
-            } else if(buttons[i].classList.contains('percent')) {
-                inputPercent(displayValue);
-                updateDisplay();
-            } else if(buttons[i].classList.contains('sign')) {
-                inputSign(displayValue);
-                updateDisplay();
-            } else if(buttons[i].classList.contains('clear'))
-                clearDisplay();
-                updateDisplay();
-        }
-    )}
+  console.log("Inside clickButton");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+      if (buttons[i].classList.contains("operand")) {
+        inputOperand(buttons[i].value);
+        updateDisplay();
+      } else if (buttons[i].classList.contains("operator")) {
+        inputOperator(buttons[i].value);
+      } else if (buttons[i].classList.contains("equals")) {
+        inputEquals();
+        updateDisplay();
+      } else if (buttons[i].classList.contains("decimal")) {
+        inputDecimal(buttons[i].value);
+        updateDisplay();
+      } else if (buttons[i].classList.contains("percent")) {
+        inputPercent(displayValue);
+        updateDisplay();
+      } else if (buttons[i].classList.contains("sign")) {
+        inputSign(displayValue);
+        updateDisplay();
+      } else if (buttons[i].classList.contains("clear")) clearDisplay();
+      updateDisplay();
+    });
+  }
 }
 
 clickButton();
 
 function inputOperand(operand) {
-    console.log("Operand in inputOperand function is:", operand);
-    console.log("Display value inside inputOperand is: ", displayValue);
-    console.log("First operand is: ", firstOperand);
-    if(firstOperator === null) {
-        if(displayValue === '0' || displayValue === 0) {
-            //1st click - handles first operand input
-            displayValue = operand;
-        } else if(displayValue === firstOperand) {
-            //starts new operation after inputEquals()
-            console.log("Inside displayValue === firstOperand");
-            displayValue = operand;
-        } else {
-            displayValue += operand;
-        }
+  console.log("Operand in inputOperand function is:", operand);
+  console.log("Display value inside inputOperand is: ", displayValue);
+  console.log("First operand is: ", firstOperand);
+  if (firstOperator === null) {
+    if (displayValue === "0" || displayValue === 0) {
+      //1st click - handles first operand input
+      displayValue = operand;
+    } else if (displayValue === firstOperand) {
+      //starts new operation after inputEquals()
+      console.log("Inside displayValue === firstOperand");
+      displayValue = operand;
     } else {
-        //3rd/5th click - inputs to secondOperand
-        if(displayValue === firstOperand) {
-            displayValue = operand;
-        } else {
-            displayValue += operand;
-        }
+      displayValue += operand;
     }
+  } else {
+    //3rd/5th click - inputs to secondOperand
+    if (displayValue === firstOperand) {
+      displayValue = operand;
+    } else {
+      displayValue += operand;
+    }
+  }
 }
 
 function inputOperator(operator) {
-    console.log("Inside inputOperator");
-    if(firstOperator != null && secondOperator === null) {
-        //4th click - handles input of second operator
-        secondOperator = operator;
-        secondOperand = displayValue;
-        result = operate(Number(firstOperand), Number(secondOperand), firstOperator);
-        displayValue = roundAccurately(result, 15).toString();
-        firstOperand = displayValue;
-        result = null;
-    } else if(firstOperator != null && secondOperator != null) {
-        //6th click - new secondOperator
-        secondOperand = displayValue;
-        result = operate(Number(firstOperand), Number(secondOperand), secondOperator);
-        secondOperator = operator;
-        displayValue = roundAccurately(result, 15).toString();
-        firstOperand = displayValue;
-        result = null;
-    } else { 
-        //2nd click - handles first operator input
-        firstOperator = operator;
-        firstOperand = displayValue;
-    }
+  console.log("Inside inputOperator");
+  if (firstOperator != null && secondOperator === null) {
+    //4th click - handles input of second operator
+    secondOperator = operator;
+    secondOperand = displayValue;
+    result = operate(
+      Number(firstOperand),
+      Number(secondOperand),
+      firstOperator
+    );
+    displayValue = roundAccurately(result, 15).toString();
+    firstOperand = displayValue;
+    result = null;
+  } else if (firstOperator != null && secondOperator != null) {
+    //6th click - new secondOperator
+    secondOperand = displayValue;
+    result = operate(
+      Number(firstOperand),
+      Number(secondOperand),
+      secondOperator
+    );
+    secondOperator = operator;
+    displayValue = roundAccurately(result, 15).toString();
+    firstOperand = displayValue;
+    result = null;
+  } else {
+    //2nd click - handles first operator input
+    firstOperator = operator;
+    firstOperand = displayValue;
+  }
 }
 
 function inputEquals() {
-    console.log("Inside inputEquals");
-    //hitting equals doesn't display undefined before operate()
-    if(firstOperator === null) {
-        displayValue = displayValue;
-    } else if(secondOperator != null) {
-        //handles final result
-        secondOperand = displayValue;
-        result = operate(Number(firstOperand), Number(secondOperand), secondOperator);
-        if(result === 'lmao') {
-            displayValue = 'lmao';
-        } else {
-            displayValue = roundAccurately(result, 15).toString();
-            firstOperand = displayValue;
-            secondOperand = null;
-            firstOperator = null;
-            secondOperator = null;
-            result = null;
-        }
+  console.log("Inside inputEquals");
+  //hitting equals doesn't display undefined before operate()
+  if (firstOperator === null) {
+    displayValue = displayValue;
+  } else if (secondOperator != null) {
+    //handles final result
+    secondOperand = displayValue;
+    result = operate(
+      Number(firstOperand),
+      Number(secondOperand),
+      secondOperator
+    );
+    if (result === "lmao") {
+      displayValue = "lmao";
     } else {
-        //handles first operation
-        secondOperand = displayValue;
-        result = operate(Number(firstOperand), Number(secondOperand), firstOperator);
-        if(result === 'lmao') {
-            displayValue = 'lmao';
-        } else {
-            displayValue = roundAccurately(result, 15).toString();
-            firstOperand = displayValue;
-            secondOperand = null;
-            firstOperator = null;
-            secondOperator = null;
-            result = null;
-        }
+      displayValue = roundAccurately(result, 15).toString();
+      firstOperand = displayValue;
+      secondOperand = null;
+      firstOperator = null;
+      secondOperator = null;
+      result = null;
     }
+  } else {
+    //handles first operation
+    secondOperand = displayValue;
+    result = operate(
+      Number(firstOperand),
+      Number(secondOperand),
+      firstOperator
+    );
+    if (result === "lmao") {
+      displayValue = "lmao";
+    } else {
+      displayValue = roundAccurately(result, 15).toString();
+      firstOperand = displayValue;
+      secondOperand = null;
+      firstOperator = null;
+      secondOperator = null;
+      result = null;
+    }
+  }
 }
 
 function inputDecimal(dot) {
-    console.log("Inside inputDecimal");
-    if(displayValue === firstOperand || displayValue === secondOperand) {
-        displayValue = '0';
-        displayValue += dot;
-    } else if(!displayValue.includes(dot)) {
-        displayValue += dot;
-    } 
+  console.log("Inside inputDecimal");
+  if (displayValue === firstOperand || displayValue === secondOperand) {
+    //displayValue = '0';
+    displayValue += dot;
+  } else if (!displayValue.includes(dot)) {
+    displayValue += dot;
+  }
 }
 
 function inputPercent(num) {
-    console.log("Inside inputPercent");
-    displayValue = (num/100).toString();
+  console.log("Inside inputPercent");
+  displayValue = (num / 100).toString();
 }
 
 function inputSign(num) {
-    console.log("Inside inputSign");
-    displayValue = (num * -1).toString();
+  console.log("Inside inputSign");
+  displayValue = (num * -1).toString();
 }
 
 function clearDisplay() {
-    console.log("Inside clearDisplay");
-    displayValue = '0';
-    firstOperand = null;
-    secondOperand = null;
-    firstOperator = null;
-    secondOperator = null;
-    result = null;
+  console.log("Inside clearDisplay");
+  displayValue = "0";
+  firstOperand = null;
+  secondOperand = null;
+  firstOperator = null;
+  secondOperator = null;
+  result = null;
 }
 
 function inputBackspace() {
-    console.log("Inside inputBackspace");
-    if(firstOperand != null) {
-        firstOperand = null;
-        updateDisplay();
-    }
+  console.log("Inside inputBackspace");
+  if (firstOperand != null) {
+    firstOperand = null;
+    updateDisplay();
+  }
 }
 
 function operate(x, y, op) {
-    console.log("Inside operate");
-    if(op === '+') {
-        return x + y;
-    } else if(op === '-') {
-        return x - y;
-    } else if(op === '*') {
-        return x * y;
-    } else if(op === '/') {
-        if(y === 0) {
-            return 'lmao';
-        } else {
-        return x / y;
-        }
+  console.log("Inside operate");
+  if (op === "+") {
+    return x + y;
+  } else if (op === "-") {
+    return x - y;
+  } else if (op === "*") {
+    return x * y;
+  } else if (op === "/") {
+    if (y === 0) {
+      return "lmao";
+    } else {
+      return x / y;
     }
+  } else if (op === "^") {
+    return x ** y;
+  }
 }
 
 function roundAccurately(num, places) {
-    console.log("Inside roundAccurately");
-    return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
+  console.log("Inside roundAccurately");
+  return parseFloat(Math.round(num + "e" + places) + "e-" + places);
 }
